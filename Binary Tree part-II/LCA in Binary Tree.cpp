5b289249529e262277be4fe0d9dda86293ab337a
Mihir Agrawal <92500568+Thinker-08@users.noreply.github.com>
2023-07-17 22:44:04 +0530

    Update LCA in Binary Tree.cpp

--- Binary Tree part-II/LCA in Binary Tree.cpp
+++ Binary Tree part-II/LCA in Binary Tree.cpp
@@ -1,29 +1,15 @@
-bool find(TreeNode* root,TreeNode* temp)
-    {
-        if(root==NULL)
-            return false;
-        if(root==temp)
-            return true;
-        else
-        {
-            if((find(root->left,temp)) or (find(root->right,temp)))
-                return true;
-            else
-                return false;
-        }
-    }
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(root==NULL)
             return NULL;
-        if(root==p or root==q)
+        if(root == p || root == q)
             return root;
-        bool lroot = (find(root->left,p)) or (find(root->left,q));
-        bool rroot = (find(root->right,q) or find(root->right,p));
-        if(lroot and rroot)
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if(left==NULL and right!=NULL)
+            return right;
+        if(right==NULL and left!=NULL)
+            return left;
+        if(right!=NULL and left!=NULL)
             return root;
-        if(lroot==false and rroot)
-            return lowestCommonAncestor(root->right,p,q);
-        if(lroot and rroot==false)
-            return lowestCommonAncestor(root->left,p,q);
         return NULL;
     }
