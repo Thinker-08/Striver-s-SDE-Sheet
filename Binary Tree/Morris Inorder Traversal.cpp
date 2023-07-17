26e1b94d66129523ad0508c4cf1e4cdd8b488fa2
Mihir Agrawal <92500568+Thinker-08@users.noreply.github.com>
2023-07-17 15:55:36 +0530

    Update Morris Inorder Traversal.cpp

--- Binary Tree/Morris Inorder Traversal.cpp
+++ Binary Tree/Morris Inorder Traversal.cpp
@@ -2,27 +2,21 @@ vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
         while(root!=NULL)
         {
-            if(root->left==NULL)
+            if(root->left!=NULL)
             {
-                ans.push_back(root->val);
-                root=root->right;
+                TreeNode* temp = root;
+                TreeNode* temp2 = root->left;
+                root=root->left;
+                while(root->right!=NULL)
+                    root=root->right;
+                root->right = temp;
+                temp->left=NULL;
+                root = temp2;
             }
             else
             {
-                TreeNode* prev=root->left;
-                while(prev->right!=NULL and prev->right!=root)
-                    prev=prev->right;
-                if(prev->right==NULL)
-                {
-                    prev->right=root;
-                    root=root->left;
-                }
-                else
-                {
-                    prev->right=NULL;
-                    ans.push_back(root->val);
-                    root=root->right;
-                }
+                ans.push_back(root->val);
+                root = root->right;
             }
         }
         return ans;
