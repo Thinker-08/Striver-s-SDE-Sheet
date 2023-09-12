f27c9c97782906836a4d735b037e89699663d4f8
Mihir Agrawal <92500568+Thinker-08@users.noreply.github.com>
2023-09-12 23:55:54 +0530

    Update Implement Trie (Prefix Tree).cpp

--- Trie/Implement Trie (Prefix Tree).cpp
+++ Trie/Implement Trie (Prefix Tree).cpp
@@ -1,64 +1,61 @@
 class Trie {
 public:
-    class Node{
-            public: 
-                int end;
-                Node* next[26];
-                Node()
-                {
-                    for(int i=0;i<26;i++)
-                        next[i]=NULL;
-                    end=false;
-                }
-        };
-    Node* tri;
+    class Node {
+        public: 
+            bool end;
+            Node* next[26];
+        Node()
+        {
+            end=false;
+            for(int i=0;i<26;i++)
+                next[i]=NULL;
+        }
+    };
+    Node* trie;
     Trie() {
-        tri=new Node();
+        trie = new Node();
     }
     
     void insert(string word) {
-        Node* temp=tri;
-        int i=0;
-        while(i<word.length())
+        Node* temp = trie;
+        for(int i=0;i<word.length();i++)
         {
             if(temp->next[word[i]-'a']==NULL)
-                temp->next[word[i]-'a']=new Node();
+                temp->next[word[i]-'a'] = new Node();
             temp=temp->next[word[i]-'a'];
-            i++;
         }
         temp->end=true;
     }
     
     bool search(string word) {
-        Node* temp=tri;
-        int i=0;
-        while(i<word.length())
+        Node* temp=trie;
+        for(int i=0;i<word.length();i++)
         {
             if(temp->next[word[i]-'a']==NULL)
                 return false;
             temp=temp->next[word[i]-'a'];
-            i++;
-        }
-        return temp->end;
-    }
-    
-    bool startsWith(string word) {
-        Node* temp=tri;
-        int i=0;
-        while(i<word.length())
-        {
-            if(temp->next[word[i]-'a']==NULL)
-                return false;
-            temp=temp->next[word[i]-'a'];
-            i++;
         }
         if(temp->end)
             return true;
-        for(int i=0;i<26;i++)
-        {
-            if(temp->next[i]!=NULL)
-                return true;
-        }
         return false;
     }
+    
+    bool startsWith(string prefix) {
+        Node* temp=trie;
+        for(int i=0;i<prefix.length();i++)
+        {
+            if(temp->next[prefix[i]-'a']==NULL)
+                return false;
+            temp=temp->next[prefix[i]-'a'];
+        }
+        return true;
+    }
 };
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
